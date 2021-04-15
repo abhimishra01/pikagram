4f84341fe459f40d34bd395336dd1a067fa85044
Moonwalker <abhisk.mhra@gmail.com>
2021-04-15 13:43:44 +0530

    WIP: new progressbar component

---
+++ src/comps/ProgressBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import useStorage from './hooks/useStorage';
+
+const ProgressBar = ()=>{
+    
+    
+    return (
+        <div className='Progressbar'>progress</div>
+
+    )
+}
+
+
+export default ProgressBar;
