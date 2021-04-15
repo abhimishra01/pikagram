01810837478e2ba3b203453455a72ea19e89dba9
Moonwalker <abhisk.mhra@gmail.com>
2021-04-15 16:12:47 +0530

    WIP: added imagegrid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Title from './comps/Title';
 import UploadForm from './comps/uploadForm';
-
+import ImageGrid from './comps/imageGrid'
 function App() {
   return (
     <div className="App">
       <Title/>
       <UploadForm/>
+      <ImageGrid/>
     </div>
   );
 }
