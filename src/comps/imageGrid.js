bf4cdfe22f566a0ff0fefbd01ca728c26c034d76
Moonwalker <abhisk.mhra@gmail.com>
2021-04-15 16:12:37 +0530

    Created new component

---
+++ src/comps/imageGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImageGrid = () =>{
+    return (
+        <div className='img-grid'>
+            images
+
+        </div>
+    );
+}
+
+export default ImageGrid;
