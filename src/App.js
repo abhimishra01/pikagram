9ed112605fc29a98ef1f316b11db3b68c8bd5b5b
Moonwalker <abhisk.mhra@gmail.com>
2021-04-15 19:09:53 +0530

    prop inside imggrid

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Title from './comps/Title';
 import UploadForm from './comps/uploadForm';
 import ImageGrid from './comps/imageGrid'
+import Modal from './comps/modal';
 function App() {
   return (
     <div className="App">
       <Title/>
       <UploadForm/>
-      <ImageGrid/>
+      <ImageGrid selectedImg={setSelectedImg} />
+      <Modal/>
     </div>
   );
 }
