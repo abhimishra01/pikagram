a1b0c457e452c5096a65df6ad5a6443060fe3b56
Moonwalker <abhisk.mhra@gmail.com>
2021-04-15 13:38:01 +0530

    updated with useEffect() method for handling the logic and change of states continuoulsly with upload, progress, error and completed events

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -29,10 +29,51 @@ const useStorage = (file)=> {
      * useEffect() will be fired everytime the dependency changes
      */
 
+    useEffect(() => {
+    
+        //    references
+        const storageRef = projectStorage.ref(file.name); // create a reference to the file inside default firebase storage
+        
+        // putting the file using that reference
+        /**
+         * storage.Ref.put(file) is a method to upload file -> asynchronous function
+         * attaching a listener to the same to fire whenever the certain event happens
+         * storage.Ref.put(file).on('state_changed') => progress changes, upload complete are diff states states
+         * we will fire another function in second argument of listener after state changes
+         */
+        storageRef.put(file).on('state_changed', (snapshot) => {
+            // snapshot is snapshot of time of that upload at that moment of time
+            // state_change event happens more than 2 3 times during the whole cycle of upload so the second argument function will run accordingly
 
+            // now we will figure out progress of  the upload 
+            // snap.bytesTransferred => property of snapshot tells us no of bytes transferred at that very moment
+            // snap.totalBytes => property to tell total size of file  
+            let percentage = (snap.bytesTransferred / snap.TotalBytes) * 100;
+            // upload percentage
+            setProgress(percentage);
 
+            
+        }, (err)=>{
+            setError(err);
+        }// third argument in this is also a function if error comes
+        ,async () => {
+            // fourth argument , a function , fires when upload is completed
+            const url = await storageRef.getDownloadURL();
+            
+            setURL(url); 
+            // both url vars are inside diff scopes in our code so none does overrides anyone
+        }
+        ) 
+        
+    }, [file]);
+
+    return {progress, url, error};
+
+    // if we set this hook inside any component, these are the values which we are going to access.
 
 }
+
+export default useStorage;
 
 
 
