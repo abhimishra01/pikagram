1f47e8bb4a3888b55ad37726d06b8a41a924338e
Moonwalker <abhisk.mhra@gmail.com>
2021-04-15 11:37:12 +0530

    imported uploadform component for testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './comps/Title';
+import UploadForm from './comps/uploadForm';
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <UploadForm/>
     </div>
   );
 }
