f28fb425ce641fd44365fbcdad94a0acb5d2233f
Moonwalker <abhisk.mhra@gmail.com>
2021-04-15 15:51:13 +0530

    minor import fix

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,5 +1,4 @@
-import React from 'react' 
-import {useEffect} from 'react';
+import React, {useEffect} from 'react';
 import useStorage from '../hooks/useStorage';
 
 const ProgressBar = ({file,setFile})=>{
