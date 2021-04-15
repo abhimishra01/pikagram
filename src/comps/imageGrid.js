2d6b717e09ac96e8027bc37294a33d27e4241402
Moonwalker <abhisk.mhra@gmail.com>
2021-04-15 19:11:23 +0530

    passing value of prop as url of selected image to set selected img, onclick event

--- src/comps/imageGrid.js
+++ src/comps/imageGrid.js
@@ -2,14 +2,16 @@ import React from 'react';
 import useFirestore from '../hooks/useFirestore';
 
 
-const ImageGrid = () =>{
+const ImageGrid = ({setSelectedImg}) =>{
 
     const { doc } = useFirestore('images');
     // console.log(doc);
     return (
         <div className='img-grid'>
            {doc && doc.map((docs) => 
-               (<div className='image-wrap' 
+               (<div
+                onClick={() =>setSelectedImg(doc.url)}
+                 className='image-wrap' 
                key={docs.id}>
                    <img class='img' src={docs.url} alt='uploaded pic'></img>
                </div>
