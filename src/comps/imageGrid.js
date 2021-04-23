bdbc6e64bd022b6999ae206f82ae4089739d03af
Moonwalker <abhisk.mhra@gmail.com>
2021-04-23 21:45:50 +0530

    WIP: LIkes function by using useLikeHook

--- src/comps/imageGrid.js
+++ src/comps/imageGrid.js
@@ -1,30 +1,48 @@
 import React from 'react';
 import useFirestore from '../hooks/useFirestore';
+import {useState} from 'react';
+import LikeButton from './likeBtn';
 import {motion} from 'framer-motion';
+import useLike from '../hooks/useLike';
+
 
 const ImageGrid = ({setSelectedImg}) =>{
-
-    const { doc } = useFirestore('images');
-    // console.log(doc);
-    return (
-        <div className='img-grid'>
+    
+    const [likes, setLikes] = useState(0);
+    const handleLikeEvt = (id) =>{
+        const {like} = useLike(id);
+        console.log(like);
+        setLikes(like);
+         }
+        const { doc } = useFirestore('images');
+        // console.log(doc);
+        return (
+            <div className='img-grid'>
            {doc && doc.map((docs) =>   
-               (<motion.div
+               (<div className="imgCont">
+                   <motion.div
             //    this attribute works whenever the images are shifted when new pic gets uploaded
-               layout
-               // hover effect on image    
-               whileHover={{opacity:1}}
-                onClick={() =>setSelectedImg(docs.url)}
-                 className='image-wrap' 
-               key={docs.id}>
+            layout
+            // hover effect on image    
+            whileHover={{opacity:1}}
+            onClick={() =>setSelectedImg(docs.url)}
+            className='image-wrap' 
+            key={docs.id}>
                    <motion.img class='img' src={docs.url} alt='uploaded pic'
                    initial={{opacity:0}}
                    animate={{opacity:1}}
                    transitio={{delay:1}}
                    />
                </motion.div>
+           <div className="btns">
+                 <LikeButton
+                 onClick={() => handleLikeEvt(docs.id)}
+                 />
+                 <h1>like</h1>
+             </div>
+               </div>
            ))}
-
+              
         </div>
     );
 }
