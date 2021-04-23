060d2fe0b95fe0ce53af4d6bf636d65b42da569d
Moonwalker <abhisk.mhra@gmail.com>
2021-04-23 21:45:12 +0530

    Like button Component

--- src/comps/likeBtn.js
+++ src/comps/likeBtn.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import {useState} from 'react';
+import Emoji from 'a11y-react-emoji'
+
 const LikeButton = () => {
+    const [likeCounter, setlikeCounter] = useState(0);
+    
+    const likeHandleEvent = ()=>{
+            setlikeCounter(likeCounter++);
+    }
+
     return ( 
         <div className="like-btn">
-            <button>Like</button>
+            <button
+            onClick={likeHandleEvent}
+            > <Emoji symbol="💕" label="love" />
+            {likeCounter}
+            </button>
         </div>
      );
 }
