ecd366a391eabcf76035671a0842b49676c83092
Moonwalker <abhisk.mhra@gmail.com>
2021-04-23 22:36:34 +0530

    removed uwnanted hook

--- src/hooks/useFetchLikes.js
+++
@@ -1,9 +0,0 @@
-import { useState, useEffect } from 'react';
-import {useLike} from './useLike';
-
-const useFetchLikes = (id) => {
-    const likes = uselike(id);
-    return {likes};
-}
- 
-export default useFetchLikes;
