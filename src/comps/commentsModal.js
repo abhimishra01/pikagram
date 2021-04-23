0be9bbf83aab317f182125a2c5ec25f670b03d08
Moonwalker <abhisk.mhra@gmail.com>
2021-04-23 23:56:07 +0530

    Comments modal

--- src/comps/commentsModal.js
+++ src/comps/commentsModal.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import {motion} from 'framer-motion';
 import {fireStore} from "../firebaseConfig";
+import  firebase from 'firebase/app'; 
+import {useState} from 'react';
 
-const CmtModal = ({selectedCmtBtn,setSelectedCmtBtn}) =>{
+const CmtModal = ({imgId,setSelectedCmtBtn}) =>{
     const databaseRef = fireStore.collection('images');
-    const handleClickEvent = ()=>{
-        databaseRef.doc(id).update({
-            likes: likes+1
-        });  
-    }
+    const [comment, setComment] = useState("");
 
-const closeBackdrop = (evt)=>{
+    const closeBackdrop = (evt)=>{
     
         // checking if the clicked is inside the image or outside the image -> we only need to close when outside the image is clicked
         if(evt.target.classList.contains('backdrop')){
-        setSelectedImg(null);}
+            setSelectedCmtBtn(null);}
+    }
+
+    const handleSubmit = (evt)=>{
+        console.log(imgId);
+        evt.preventDefault();
+        databaseRef.doc(imgId).update({
+            commments: firebase.firestore.FieldValue.arrayUnion(comment)
+        });  
+        closeBackdrop();
     }
 
     return (
@@ -22,12 +29,17 @@ const closeBackdrop = (evt)=>{
         initial={{opacity:0}}
         animate={{opacity:1}}
         onClick={closeBackdrop}>
-            {/* <motion.img
-            // this property takes the img off the screen on y axis and then animates accordingly
+            <motion.form
+        className="comments-form"
+        onSubmit={handleSubmit}
             initial={{y:"-100vh"}}
-            animate={{y:0}}
-            src={selectedImg} alt="enlarged"/> */}
-        <textarea name="" id="" cols="30" rows="10">Input here</textarea>
+            animate={{y:0}}>
+        <textarea
+         onChange={(event)=>setComment(event.target.value)}
+        name="CommentBox" id="" cols="60" rows="10"
+      placeholder="comment"></textarea>
+        <button>Comment</button>
+        </motion.form>
         </motion.div>
     )
 }
