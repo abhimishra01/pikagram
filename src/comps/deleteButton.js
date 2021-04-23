80f15e9c0d948c7d9156e4e2725a7f5b764df42e
Moonwalker <abhisk.mhra@gmail.com>
2021-04-23 22:39:32 +0530

    feat: fully functional delete button

--- src/comps/deleteButton.js
+++ src/comps/deleteButton.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import Emoji from 'a11y-react-emoji';
 import {fireStore} from "../firebaseConfig";
-import {useState} from 'react';
 
 const DeleteBtn = ({id}) => {
-
-    const [isInProgress, setisInProgress] = useState(false);
     const databaseRef = fireStore.collection('images');
     const likeHandleEvent = ()=>{
-        setisInProgress(true);
         databaseRef.doc(id).delete()
         .then(()=>{
-            setisInProgress(false);
             console.log("Deleted Successfully");
         })
     }
@@ -19,7 +14,7 @@ const DeleteBtn = ({id}) => {
             <button
             className="like-btn"
             onClick={likeHandleEvent}
-            > {isInProgress?"Deleting..":<Emoji symbol="❌" label="delete" />}</button>
+            > <Emoji symbol="❌" label="delete" /></button>
        
      );
 }
