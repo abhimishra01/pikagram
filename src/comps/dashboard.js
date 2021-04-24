fc0bea93668df15eca6f0a3178e6f4073f5433ec
Moonwalker <abhisk.mhra@gmail.com>
2021-04-24 11:42:21 +0530

    Dashboard after signUp

---
+++ src/comps/dashboard.js
@@ -0,0 +1,34 @@
+import React,{useState} from 'react';
+import Title from './comps/Title';
+import UploadForm from './comps/uploadForm';
+import ImageGrid from './comps/imageGrid'
+import Modal from './comps/modal';
+import CmtModal from './comps/commentsModal';
+
+const Dashboard = ()=> {
+  const [selectedImg, setSelectedImg] = useState(null);
+  const [selectedCmtBtn, setSelectedCmtBtn] = useState(null);
+  const [comments, setComments] = useState(null);
+  const [imgId, setimgId] = useState(0)
+
+  return (
+    <div>
+      <Title/>
+      <UploadForm/>
+      <ImageGrid 
+      setComments={setComments}
+      setimgId={setimgId}
+      setSelectedImg={setSelectedImg}setSelectedCmtBtn={setSelectedCmtBtn} />
+      {selectedImg && <Modal
+      setSelectedImg={setSelectedImg}
+      selectedImg={selectedImg}/>}
+
+      {selectedCmtBtn && <CmtModal
+      comments={comments}
+      imgId={imgId}
+      setSelectedCmtBtn={setSelectedCmtBtn}/>}
+    </div>
+  );
+}
+
+export default Dashboard;
