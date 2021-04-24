ffd281ff8dd946b82003fdc2d20e75be74da75e4
Moonwalker <abhisk.mhra@gmail.com>
2021-04-24 15:44:05 +0530

    keys

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,14 +5,22 @@ import 'firebase/storage'; // also importing storage SDK will be using to store 
 import 'firebase/firestore'; // importing firestore to manage real time db stuffs    
 
 
+// var firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID
+// };
 var firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
+    apiKey: "AIzaSyBow9GhW3HdlIYCS_yymsBbArgFA_CjpUw",
+    authDomain: "pikagram-prod.firebaseapp.com",
+    projectId: "pikagram-prod",
+    storageBucket: "pikagram-prod.appspot.com",
+    messagingSenderId: "698785078930",
+    appId: "1:698785078930:web:12c3e20774ac6aa721d04b"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
