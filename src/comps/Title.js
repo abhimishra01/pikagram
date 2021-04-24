5fe7de5271991b3907f68aecd99efdbd318165b8
Moonwalker <abhisk.mhra@gmail.com>
2021-04-24 13:47:58 +0530

    added logout button comp

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import LogoutBtn from './logoutBtn';
 
 const Title = () => {
   return (
     <div className="title">
-      <h1>Pikagram</h1>
+      <div className="upperCont">
+        <h1>Pikagram</h1>
+        <LogoutBtn/>
+        </div>
       <h2>Put your best moments here</h2>
       <p>The best moments are the unplanned ones that just happen but make your day perfect !</p>
     </div>
