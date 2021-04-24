ee5bd20f9fb85fd0a6ce8a7e9f5102ef1d90eaaf
Moonwalker <abhisk.mhra@gmail.com>
2021-04-24 10:53:18 +0530

    added signin comp

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import React,{useState} from 'react';
-import Title from './comps/Title';
-import UploadForm from './comps/uploadForm';
-import ImageGrid from './comps/imageGrid'
-import Modal from './comps/modal';
-import CmtModal from './comps/commentsModal';
-
+import React from 'react';
+import SignUpForm from './comps/signUp';
 function App() {
-  const [selectedImg, setSelectedImg] = useState(null);
-  const [selectedCmtBtn, setSelectedCmtBtn] = useState(null);
-  const [comments, setComments] = useState(null);
-  const [imgId, setimgId] = useState(0)
 
   return (
     <div className="App">
-      <Title/>
-      <UploadForm/>
-      <ImageGrid 
-      setComments={setComments}
-      setimgId={setimgId}
-      setSelectedImg={setSelectedImg}setSelectedCmtBtn={setSelectedCmtBtn} />
-      {selectedImg && <Modal
-      setSelectedImg={setSelectedImg}
-      selectedImg={selectedImg}/>}
-
-      {selectedCmtBtn && <CmtModal
-      comments={comments}
-      imgId={imgId}
-      setSelectedCmtBtn={setSelectedCmtBtn}/>}
-    </div>
+      <SignUpForm/>
+      </div>
   );
 }
 
