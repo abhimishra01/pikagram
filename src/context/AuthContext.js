94881f95c72ef4ea73f7e097727d9ba9cc4a318a
Moonwalker <abhisk.mhra@gmail.com>
2021-04-24 11:00:48 +0530

    Auth Context Exported all reqd values

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import React,{useContext, useState} from 'react';
+import React,{useContext, useState,useEffect} from 'react';
 import {firebaseAuth} from "../firebaseConfig";
 
 
@@ -12,11 +12,27 @@ export default useAuth;
 
 const AuthProvider = ( {children}) => {
    
+    function signUp(email,paswd){
+      return  firebaseAuth.createUserWithEmailAndPassword(email,paswd)
+    }
+    
+
+    
     const [currUser, setcurrUser] = useState(null);
 
     const value={
-        currUser
+        currUser,
+        signUp
     }
+
+
+    useEffect(() => {
+        // we are declaring it with unsubscribe bcoz the following function returns a method with unsubscribes the listener whenever called
+   const unsubscribe =firebaseAuth.onAuthStateChanged(user=>{
+        setcurrUser(user);
+    })
+    return unsubscribe
+    }, [])
 
     return ( 
         <AuthContext.Provider value={value}>
