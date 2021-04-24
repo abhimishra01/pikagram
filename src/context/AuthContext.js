b5a19fbd55ca013b0ba282ef752bc5df0d94c528
Moonwalker <abhisk.mhra@gmail.com>
2021-04-24 11:39:04 +0530

    authContext work done

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,39 +1,62 @@
-import React,{useContext, useState,useEffect} from 'react';
-import {firebaseAuth} from "../firebaseConfig";
+import React, { useContext, useState, useEffect } from "react"
+import { auth } from "../firebaseConfig";
 
+const AuthContext = React.createContext()
 
-const AuthContext = React.createContext();
-
-const useAuth = () => {
-    return useContext(AuthContext);
+export function useAuth() {
+  return useContext(AuthContext)
 }
- 
-export default useAuth;
 
-const AuthProvider = ( {children}) => {
-   function signUp(email,paswd){
-      return  firebaseAuth.createUserWithEmailAndPassword(email,paswd)
-    }
-    const [currUser, setcurrUser] = useState(null);
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState()
+  const [loading, setLoading] = useState(true)
 
-    const value={
-        currUser,
-        signUp
-    }
-    useEffect(() => {
-        // we are declaring it with unsubscribe bcoz the following function returns a method with unsubscribes the listener whenever called
-   const unsubscribe =firebaseAuth.onAuthStateChanged(user=>{
-        setcurrUser(user);
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password)
+  }
+
+  function logout() {
+    return auth.signOut()
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email)
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email)
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password)
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+      setLoading(false)
     })
+
     return unsubscribe
-    }, [])
+  }, [])
 
-    return ( 
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
+  const value = {
+    currentUser,
+    login,
+    signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword
+  }
 
-     );
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
 }
- 
-export default AuthProvider;
