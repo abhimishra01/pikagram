2d5837d163af147e4508cb6040a9a7fba2695cbf
Moonwalker <abhisk.mhra@gmail.com>
2021-04-24 10:53:42 +0530

    using Env vars for firebase keys

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,16 +1,17 @@
 
-import  firebase from 'firebase/app';  // importing everything from firebase 
+import  firebase from 'firebase/app';  
+import 'firebase/auth'
 import 'firebase/storage'; // also importing storage SDK will be using to store our images
 import 'firebase/firestore'; // importing firestore to manage real time db stuffs    
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAL9yvrG36Xx4wcTEzpMzjjEISX_e5vORY",
-    authDomain: "pikagram-26525.firebaseapp.com",
-    projectId: "pikagram-26525",
-    storageBucket: "pikagram-26525.appspot.com",
-    messagingSenderId: "284231649349",
-    appId: "1:284231649349:web:73577a32b2c9339fcb7b2c"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -25,7 +26,10 @@ const fireStore = firebase.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
 
+// firebase auth init
+const firebaseAuth = firebase.auth();
+
 // exporting the above to use in diff react components
-export {projectStorage, fireStore, timestamp}
+export {projectStorage, fireStore, timestamp, firebaseAuth}
 
 // after this we need to initialize / configure the two services on our firebase dashboard ..
